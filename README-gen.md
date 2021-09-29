e47d96f97ed9151e8d5afa34825453aadeb2042d
aorich <rich.aidan@gmail.com>
2021-09-29 19:21:53 -0400

    a lot of readme working

--- README-gen.md
+++ README-gen.md
@@ -1,1 +1,10 @@
-{"description":"bob","install":"bob2","instructions":"bob3","collab":"bob4","license":[]}
+# quiz
+## Description
+- The goal was to create a quiz game that would test our knowledge of JavaScript.
+## Installation
+- None
+## Usage
+- When finished you will be asked to enter your name. On further play throughs your previous score will be displayed below.
+## Credits
+- Lots of help from Lief Hetland, my instructor as well as my tutor Dru Sanchez.
+        
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the project name?',
+        name: 'name',
+    },
     {
         type: 'input',
         message: 'Provide a short description explaining the what, why, and how of your project.',
@@ -42,7 +48,21 @@ inquirer
 // function writeToFile('README-gen.md', data) {}
     .then((response) => {
 
-        fs.writeFile('README-gen.md', JSON.stringify(response), (err) =>
+        fs.writeFile('README-gen.md', `# ${response.name}
+## Description
+- ${response.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+- ${response.install}
+## Usage
+- ${response.instructions}
+## Credits
+- ${response.collab}
+        `, (err) =>
             // TODO: Describe how this ternary operator works
             err ? console.error(err) : console.log('Commit logged!')
 
