b133f65185d17440af8d2b49c601b6e2967e0392
aorich <rich.aidan@gmail.com>
2021-09-29 09:40:30 -0400

    Added array

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Provide a short description explaining the what, why, and how of your project.",
+    "What are the steps required to install your project?",
+    "Provide instructions and examples for use.",
+    "List your collaborators, if any, with links to their GitHub profiles.",
+    "The last section of a high-quality README file is the license."
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
